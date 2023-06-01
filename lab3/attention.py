fb1cb2e9372906aa8db8a712fffee434025dc704
pxaris <pxaris@gmail.com>
2023-06-01 17:44:52 +0300

    add MultiHeadAttention models

--- lab3/attention.py
+++ lab3/attention.py
@@ -84,3 +84,37 @@ class SimpleSelfAttentionModel(nn.Module):
 
         logits = self.output(x)  # (C,output)
         return logits
+
+
+class MultiHeadAttention(nn.Module):
+    """ multiple heads of self-attention in parallel """
+
+    def __init__(self, num_heads, head_size, n_embd, dropout=0.0):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size, n_embd)
+                                    for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
+
+
+class MultiHeadAttentionModel(nn.Module):
+
+    def __init__(self, output_size, embeddings, max_length=60, n_head=3):
+        super().__init__()
+        
+        # TODO: Main-Lab-Q4 - define the model
+        # Hint: it will be similar to `SimpleSelfAttentionModel` but
+        # `MultiHeadAttention` will be utilized for the self-attention module here        
+        ...
+
+
+    def forward(self, x):
+        ...
+
+        logits = ...
+        return logits
