f7ca48ea1ecbdfe74c85891b7061c62d5d6c9e73
pxaris <pxaris@gmail.com>
2023-06-01 16:02:09 +0300

    LSTM base implementation

--- lab3/models.py
+++ lab3/models.py
@@ -1,5 +1,5 @@
 import torch
-
+import numpy as np
 from torch import nn
 
 
@@ -61,5 +61,51 @@ class BaselineDNN(nn.Module):
 
         # 4 - project the representations to classes using a linear layer
         logits = ...  # EX6
+
+        return logits
+
+
+class LSTM(nn.Module):
+    def __init__(self, output_size, embeddings, trainable_emb=False, bidirectional=False):
+
+        super(LSTM, self).__init__()
+        self.hidden_size = 100
+        self.num_layers = 1
+        self.bidirectional = bidirectional
+
+        self.representation_size = 2 * \
+            self.hidden_size if self.bidirectional else self.hidden_size
+
+        embeddings = np.array(embeddings)
+        num_embeddings, dim = embeddings.shape
+
+        self.embeddings = nn.Embedding(num_embeddings, dim)
+        self.output_size = output_size
+
+        self.lstm = nn.LSTM(dim, hidden_size=self.hidden_size,
+                            num_layers=self.num_layers, bidirectional=self.bidirectional)
+
+        if not trainable_emb:
+            self.embeddings = self.embeddings.from_pretrained(
+                torch.Tensor(embeddings), freeze=True)
+
+        self.linear = nn.Linear(self.representation_size, output_size)
+
+    def forward(self, x, lengths):
+        batch_size, max_length = x.shape
+        embeddings = self.embeddings(x)
+        X = torch.nn.utils.rnn.pack_padded_sequence(
+            embeddings, lengths, batch_first=True, enforce_sorted=False)
+
+        ht, _ = self.lstm(X)
+
+        # ht is batch_size x max(lengths) x hidden_dim
+        ht, _ = torch.nn.utils.rnn.pad_packed_sequence(ht, batch_first=True)
+
+        # pick the output of the lstm corresponding to the last word
+        # TODO: Main-Lab-Q2 (Hint: take actual lengths into consideration)
+        representations = ...
+
+        logits = self.linear(representations)
 
         return logits
